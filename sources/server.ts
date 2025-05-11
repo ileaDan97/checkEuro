ce27421a9ad2221b00a77a699dd6c68b0da93eb9
ilea.dan <ileadan97@gmail.com>
2025-05-11 18:09:48 +0300

    added secrets

--- sources/server.ts
+++ sources/server.ts
@@ -1,18 +1,46 @@
-import { chromium } from 'playwright';
+import {chromium} from 'playwright';
+// @ts-ignore
+import nodemailer from 'nodemailer';
+// @ts-ignore
+import dotenv from 'dotenv';
+dotenv.config();
+
 let checkEuroValue = async () => {
-    const browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({headless: true});
     const page = await browser.newPage();
 
     await page.goto("https://transilvaniaexchange.ro/")
     const sellEuroText = await page.locator(".row-2.even .column-5").textContent()
-    console.log(sellEuroText)
 
     // Clean and convert the string to a number
     const sellEuroRaw = parseFloat(sellEuroText?.replace(",", ".") || "0")
 
-    console.log(sellEuroRaw)
+    return sellEuroRaw
 }
 
+let sendEmail = async () => {
+    const currentExchange = await checkEuroValue()
+    const transporter = nodemailer.createTransport({
+        service: 'yahoo',
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD, // NOT your Gmail password!
+        },
+    });
 
+    const mailOptions = {
+        from: process.env.USER,
+        to: process.env.USER,
+        subject: 'check daily Euro rate',
+        text: `Euro current exchange is ${currentExchange}`
+    };
 
-checkEuroValue()
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.messageId);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
+sendEmail()
